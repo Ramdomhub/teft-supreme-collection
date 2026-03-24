ffb31ffa6a123aa1c53d5151e4fd526180f29bc5
Ramdomhub <cn_1@gmx.at>
2026-03-24 15:49:45 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 export const metadata = {
-  title: "TEFT Supreme",
-  description: "Inside X.",
-};
+  title: "TEFT",
+  description: "Explore the ecosystem",
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+  openGraph: {
+    title: "TEFT",
+    description: "Explore the ecosystem",
+    url: "https://teft-supreme-collection.vercel.app/teft-token",
+    siteName: "TEFT",
+    images: [
+      {
+        url: "https://teft-supreme-collection.vercel.app/teft.png",
+        width: 1200,
+        height: 1200,
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "TEFT",
+    description: "Explore the ecosystem",
+    images: ["https://teft-supreme-collection.vercel.app/teft.png"],
+  },
+};
